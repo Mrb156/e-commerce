bc0659da62f49a039dc5c4f8675b7fb62869c975
Morvai Barna <morvaibarna@gmail.com>
2023-05-18 15:52:09 +0200

    checkout with empty basket issue fixed

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use function Webmozart\Assert\Tests\StaticAnalysis\length;
 
 class CheckoutController extends Controller
 {
@@ -19,12 +20,16 @@ class CheckoutController extends Controller
         } else {
             $orderId = null;
         }
-
-        return view('checkout', [
-            'categories' => DB::table('categories')->select('*')->get(),
-            'subcategories' => DB::table('sub_categories')->select('*')->get(),
-            'order_items' => DB::table('order_items')->select('*')->where('order_id', '=', $orderId)->get(),
-            'order' => DB::table('orders')->select('*')->where('id', '=', $orderId)->first(),
-        ]);
+        $order_items = DB::table('order_items')->select('*')->where('order_id', '=', $orderId)->get();
+        if ($order_items->isEmpty()) {
+            return redirect(route('home'))->with('message', 'Nincs semmi a kosaradban.');
+        } else {
+            return view('checkout', [
+                'categories' => DB::table('categories')->select('*')->get(),
+                'subcategories' => DB::table('sub_categories')->select('*')->get(),
+                'order_items' => $order_items,
+                'order' => DB::table('orders')->select('*')->where('id', '=', $orderId)->first(),
+            ]);
+        }
     }
 }
